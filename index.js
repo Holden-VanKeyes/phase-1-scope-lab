5b09365fcdd8499477f1408a59155435d318c152
Abram Shook <ajshook77@gmail.com>
2022-04-29 17:41:51 -0500

    commit tree 03a71a447cad6ceb49a026246a44573dd6afee6f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+// leastFavoriteCustomer and changeLeastFavoriteCustomer():
+// Now declare a constant (using the const keyword) in global scope called leastFavoriteCustomer;
+// be sure to assign it some initial value.
+// Finally, write a function called changeLeastFavoriteCustomer()
+// that attempts to change that constant -
+// notice what JavaScript does when you try to change the constant.
+
+var customerName = 'bob'
+//const upperCaseCustomerName = (customerName) => customerName.toUpperCase()
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+var bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer
+}
+const leastFavoriteCustomer = 'yo'
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'sam'
+}
+
+// var bestCustomer = function () {
+//   var setBestCustomer = 'not bob'
+//   return setBestCustomer
+// }
+
+// function setBestCustomer() {
+//   var bestCustomer = customerName
+//   return 'not', bestCustomer
+// }
